1ff5ce0517d81562d90bba213d5f153afd837a55
r4gh4v <raghavxb@gmail.com>
2018-03-02 16:44:50 -0800

    Navigation Drawer Implemented.

--- app/src/main/java/com/example/android/getorganized/MainActivity.java
+++ app/src/main/java/com/example/android/getorganized/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.getorganized;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnstart=(Button) findViewById(R.id.getstarted);
+
+        btnstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in=new Intent(MainActivity.this,navdrawer.class);
+                startActivity(in);
+            }
+        });
     }
 }
