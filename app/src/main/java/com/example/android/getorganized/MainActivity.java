296ae9daeaa1b53fda07ed441cc7be62ed8b4065
nisarg1995 <nisargsohagiya007@gmail.com>
2018-03-03 16:22:56 -0800

    nisarg

--- app/src/main/java/com/example/android/getorganized/MainActivity.java
+++ app/src/main/java/com/example/android/getorganized/MainActivity.java
@@ -1,27 +1,30 @@
 package com.example.android.getorganized;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Switch;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnstart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findviewByid();
+    }
 
-        Button btnstart=(Button) findViewById(R.id.getStarted);
-
-        btnstart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent in=new Intent(MainActivity.this,navdrawer.class);
-                startActivity(in);
-            }
-        });
+    private void findviewByid() {
+        btnstart= (Button) findViewById(R.id.getStarted);
     }
 
     public void maleClick(View view) {
@@ -31,8 +34,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void englishClick(View view) {
+        setLocale("en");
     }
 
     public void chineseClick(View view) {
+        setLocale("zh");
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId())
+        {
+            case R.id.getStarted:
+                Intent in=new Intent(MainActivity.this,navdrawer.class);
+                startActivity(in);
+                break;
+        }
+
+
+    }
+
+    public void setLocale(String lang)
+    {
+        Locale locale= new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = locale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, MainActivity.class);
+        startActivity(refresh);
+
+        finish();
     }
 }
