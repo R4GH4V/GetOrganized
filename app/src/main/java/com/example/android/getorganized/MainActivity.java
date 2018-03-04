ecb28480d5e8543ef5cc38088dfb1169bd0e7a9f
nisarg1995 <nisargsohagiya007@gmail.com>
2018-03-03 19:13:12 -0800

    nisarg

--- app/src/main/java/com/example/android/getorganized/MainActivity.java
+++ app/src/main/java/com/example/android/getorganized/MainActivity.java
@@ -1,37 +1,54 @@
 package com.example.android.getorganized;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
-import android.widget.Switch;
+import android.widget.RadioGroup;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnstart;
+    private RadioButton eng,chi;
+    private RadioGroup rgroup;
+    private static final Integer checked = null;
+    public MainActivity() {
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String current = Locale.getDefault().getDisplayLanguage();
+        Log.e("current",current);
         setContentView(R.layout.activity_main);
         findviewByid();
         setonclicklistener();
     }
 
-    private void setonclicklistener() {
-        btnstart.setOnClickListener(this);
-    }
 
     private void findviewByid() {
         btnstart= (Button) findViewById(R.id.getStarted);
+        eng =(RadioButton)findViewById(R.id.eng);
+        chi =(RadioButton)findViewById(R.id.chi);
+        rgroup=(RadioGroup)findViewById(R.id.group);
     }
 
+    private void setonclicklistener() {
+        btnstart.setOnClickListener(this);
+        eng.setOnClickListener(this);
+        chi.setOnClickListener(this);
+    }
     public void maleClick(View view) {
     }
 
@@ -55,11 +72,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent in=new Intent(MainActivity.this,navdrawer.class);
                 startActivity(in);
                 break;
+
+            case R.id.eng:
+                setLocale("en");
+                break;
+
+            case R.id.chi:
+                setLocale("zh");
+                break;
         }
-
-
     }
-
     public void setLocale(String lang)
     {
         Locale locale= new Locale(lang);
@@ -68,8 +90,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Configuration conf = res.getConfiguration();
         conf.locale = locale;
         res.updateConfiguration(conf, dm);
-        Intent refresh = new Intent(this, MainActivity.class);
-        startActivity(refresh);
-        finish();
     }
 }
