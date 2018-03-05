b32b0d8d12d2194320e873c522ed1d3deafe157b
shelleywxl <xuelu_wu@hotmail.com>
2018-03-04 20:05:21 -0800

    Rename Navdrawer to NavdrawerActivity (more clear to know it is an activity).
    "get started" button goes to closet fragment, not an empty fragment.
    
    Add NavDrawerBaseActivity, now any activity extends NavDrawerBaseActivity
    will have the navigation drawer, which might be useful for later use.
    
    Add MenuFragment and its submenu (StatisticsFragment, SettingsFragment,
    HelpFragment and FeedbackFragment).
    
    Some content will be edited later.
    
    FeedbackFragment is a dialog fragment. need to be done later.

--- app/src/main/java/com/example/android/getorganized/NavdrawerActivity.java
+++ app/src/main/java/com/example/android/getorganized/NavdrawerActivity.java
@@ -17,18 +17,18 @@ public class NavdrawerActivity extends NavDrawerBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navdrawer);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+//        setContentView(R.layout.activity_navdrawer);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+//
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
 
         FragmentManager fragmentManager=getSupportFragmentManager();
         FragmentTransaction ft=fragmentManager.beginTransaction();
@@ -36,44 +36,44 @@ public class NavdrawerActivity extends NavDrawerBaseActivity {
         ft.commit();
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        Fragment fragment=null;
-
-        int id = item.getItemId();
-
-        if (id == R.id.closet) {
-            fragment=new ClosetFragment();
-        }  else if (id == R.id.calendar) {
-            fragment=new CalendarFragment();
-        } else if (id == R.id.shop) {
-            fragment=new ShopFragment();
-        } else if (id == R.id.menu) {
-            fragment=new MenuFragment();
-        }
-
-        if(fragment!=null){
-            FragmentManager fragmentManager=getSupportFragmentManager();
-            FragmentTransaction ft=fragmentManager.beginTransaction();
-            ft.replace(R.id.screen_area, fragment);
-            ft.commit();
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
+//    @Override
+//    public void onBackPressed() {
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
+//
+//
+//    @SuppressWarnings("StatementWithEmptyBody")
+//    @Override
+//    public boolean onNavigationItemSelected(MenuItem item) {
+//        // Handle navigation view item clicks here.
+//        Fragment fragment=null;
+//
+//        int id = item.getItemId();
+//
+//        if (id == R.id.closet) {
+//            fragment=new ClosetFragment();
+//        }  else if (id == R.id.calendar) {
+//            fragment=new CalendarFragment();
+//        } else if (id == R.id.shop) {
+//            fragment=new ShopFragment();
+//        } else if (id == R.id.menu) {
+//            fragment=new MenuFragment();
+//        }
+//
+//        if(fragment!=null){
+//            FragmentManager fragmentManager=getSupportFragmentManager();
+//            FragmentTransaction ft=fragmentManager.beginTransaction();
+//            ft.replace(R.id.screen_area, fragment);
+//            ft.commit();
+//        }
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        drawer.closeDrawer(GravityCompat.START);
+//        return true;
+//    }
 }
