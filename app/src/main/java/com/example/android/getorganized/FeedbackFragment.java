82c87d19f76e5a4ba4c530322d36f49d310abd03
r4gh4v <raghavxb@gmail.com>
2018-03-05 14:05:25 -0800

    Email_feedback intent added.

--- app/src/main/java/com/example/android/getorganized/FeedbackFragment.java
+++ app/src/main/java/com/example/android/getorganized/FeedbackFragment.java
@@ -1,5 +1,7 @@
 package com.example.android.getorganized;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,10 +12,12 @@ import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import java.net.URI;
+
 
 public class FeedbackFragment extends Fragment {
     @Nullable
-    @Override
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_feedback,null);
     }
@@ -25,11 +29,24 @@ public class FeedbackFragment extends Fragment {
         final RatingBar ratingRatingBar = (RatingBar) getView().findViewById(R.id.rating_rating_bar);
         Button submitButton = (Button) getView().findViewById(R.id.submit_button);
         final TextView ratingDisplayTextView = (TextView) getView().findViewById(R.id.rating_display_text_View);
-
+        Button email=(Button) getView().findViewById(R.id.email);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ratingDisplayTextView.setText(getResources().getString(R.string.yourrating)+" "+ ratingRatingBar.getRating());
+            }
+        });
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                intent.setData(Uri.parse("mailto:"));
+                String[] to={"r2bhandari@scu.edu","xwu2@scu.edu","nsohagiya@scu.edu"};
+                intent.putExtra(Intent.EXTRA_EMAIL, to);
+                intent.putExtra(Intent.EXTRA_SUBJECT,"App Feedback");
+                intent.setType("message/rfc822");
+                Intent chooser=Intent.createChooser(intent,"Send Email");
+                startActivity(chooser);
             }
         });
     }
