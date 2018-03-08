03ccd05109566c4aa2a140f198439cfd9f157a12
nisarg1995 <nisargsohagiya007@gmail.com>
2018-03-08 02:39:47 -0800

    Merge branch 'master' of D:\chrome download\GetOrganized with conflicts.

--- app/src/main/java/com/example/android/getorganized/MainActivity.java
+++ app/src/main/java/com/example/android/getorganized/MainActivity.java
@@ -3,42 +3,56 @@ package com.example.android.getorganized;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnstart;
-    private RadioButton eng,chi;
+    private RadioButton eng,chi,male,female;
     private RadioGroup rgroup;
+    private EditText email;
     private static final Integer checked = null;
+    private  DatabaseHandler db;
+    private String gender= null;
     public MainActivity() {
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        String current = Locale.getDefault().getDisplayLanguage();
-        Log.e("current",current);
-        setContentView(R.layout.activity_main);
-        findviewByid();
-        setonclicklistener();
-    }
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            db= new DatabaseHandler(MainActivity.this);
+            db.userdata();
+            boolean check= db.userdata_check();
+            if (check)
+            {
+                Intent in = new Intent(MainActivity.this, NavdrawerActivity.class);
+                startActivity(in);
+            }
+            setContentView(R.layout.activity_main);
+            findviewByid();
+            setonclicklistener();
+        }
 
 
     private void findviewByid() {
-        btnstart= (Button) findViewById(R.id.getStarted);
-        eng =(RadioButton)findViewById(R.id.eng);
-        chi =(RadioButton)findViewById(R.id.chi);
-        rgroup=(RadioGroup)findViewById(R.id.group);
+        btnstart = (Button) findViewById(R.id.getStarted);
+        eng = (RadioButton) findViewById(R.id.eng);
+        chi = (RadioButton) findViewById(R.id.chi);
+        rgroup = (RadioGroup) findViewById(R.id.group);
+        email=(EditText)findViewById(R.id.et_email);
     }
 
     private void setonclicklistener() {
@@ -46,24 +60,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         eng.setOnClickListener(this);
         chi.setOnClickListener(this);
     }
-    
-    public void englishClick(View view) {
-        setLocale("en");
-    }
 
-    public void chineseClick(View view) {
-        setLocale("zh");
-    }
+    public void maleClick(View view) {gender="Male";}
+
+    public void femaleClick(View view) {gender="Female";}
 
 
     @Override
     public void onClick(View v) {
 
-        switch(v.getId())
-        {
+        switch (v.getId()) {
+
             case R.id.getStarted:
-                Intent in=new Intent(MainActivity.this,NavdrawerActivity.class);
-                startActivity(in);
+
+                if(email.getText()!=null)
+                {
+                    if(gender=="Male" || gender=="Female")
+                    {
+                        String mail =email.getText().toString().trim();
+                        db.adduserdata(mail,gender);
+                        Intent in = new Intent(MainActivity.this, NavdrawerActivity.class);
+                        startActivity(in);
+                    }
+                    else
+                    {
+                        Toast.makeText(this, R.string.gender_error,Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                }
+                else
+                {
+                    if(email.getText()==null)
+                    {
+                        Toast.makeText(this, R.string.email_error,Toast.LENGTH_LONG).show();
+                    }
+                }
+
                 break;
 
             case R.id.eng:
@@ -75,10 +107,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
-    
-    public void setLocale(String lang)
-    {
-        Locale locale= new Locale(lang);
+
+    public void setLocale(String lang) {
+        Locale locale = new Locale(lang);
         Resources res = getResources();
         DisplayMetrics dm = res.getDisplayMetrics();
         Configuration conf = res.getConfiguration();
@@ -86,3 +117,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         res.updateConfiguration(conf, dm);
     }
 }
+
