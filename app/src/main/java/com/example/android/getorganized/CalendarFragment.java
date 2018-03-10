ed3370b371ca1120e61acd78349a7bc51e698ac6
shelleywxl <xuelu_wu@hotmail.com>
2018-03-09 17:11:01 -0800

    Add toolbar to submenus and change the theme color.

--- app/src/main/java/com/example/android/getorganized/CalendarFragment.java
+++ app/src/main/java/com/example/android/getorganized/CalendarFragment.java
@@ -15,6 +15,9 @@ public class CalendarFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        getActivity().setTitle(R.string.calendar);
+
         return inflater.inflate(R.layout.fragment_calendar,null);
     }
 
