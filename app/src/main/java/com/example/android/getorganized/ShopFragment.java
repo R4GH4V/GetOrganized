e390455a0eb9f51a12fbf3a675a9f9287fd4a3a5
r4gh4v <raghavxb@gmail.com>
2018-03-10 15:37:23 -0800

    Added GridView in Shop Fragment with (Image+Text). Clicking on image leads to Amazon web page. 81 Images added in res/raw Folder. values->shop_arrays to store the labels and urls.
    Bug: If orientation is changed ShopFrag changes to ClosetFrag. Need to fix that.

--- app/src/main/java/com/example/android/getorganized/ShopFragment.java
+++ app/src/main/java/com/example/android/getorganized/ShopFragment.java
@@ -1,23 +1,51 @@
 package com.example.android.getorganized;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 
 public class ShopFragment extends Fragment {
     @Nullable
-    @Override
+
+    String[] labels;
+    String[] pages;
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         getActivity().setTitle(R.string.shop);
-
         return inflater.inflate(R.layout.fragment_shop,null);
+    }
 
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //Getting the resources from values->shop_arrays and putting them in String arrays.
+        Resources res= getResources();
+        labels=res.getStringArray(R.array.headings);
+        pages=res.getStringArray(R.array.pages);
+
+
+
+        GridView grid= view.findViewById(R.id.shopGrid);
+        ShopGridViewAdapter myAdapter = new ShopGridViewAdapter(getActivity(), labels);
+        grid.setAdapter(myAdapter);
+
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                Intent webPageIntent = new Intent(getActivity(), ShopDisplayActivity.class);
+                webPageIntent.putExtra("WEB_PAGE", pages[ position ] );
+                startActivity( webPageIntent );
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/android/getorganized/ShopGridViewAdapter.java
@@ -0,0 +1,81 @@
+package com.example.android.getorganized;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+
+public class ShopGridViewAdapter extends BaseAdapter {
+
+    //Add the images here, in the same order you give the Label and pageURL.
+    private Integer[] shopImages = {
+            R.raw.blazers1,
+            R.raw.blazers2,
+            R.raw.blazers3,
+            R.raw.coats1,
+            R.raw.coats2,
+            R.raw.coats3,
+            R.raw.dress1,
+            R.raw.dress2,
+            R.raw.dress3,
+            R.raw.shirtsm1,
+            R.raw.shirtsm2,
+            R.raw.shirtsm3,
+            R.raw.shirtsf1,
+            R.raw.shirtsf2,
+            R.raw.shirtsf3,
+            R.raw.sweaterm1,
+            R.raw.sweaterm2,
+            R.raw.sweaterm3
+    };
+
+    private Context context;
+    private String[] imageLabels;
+
+    public ShopGridViewAdapter(Context con, String[] labs) {
+        this.context=con;
+        this.imageLabels = labs;
+    }
+
+
+
+    @Override
+    public int getCount() {
+        return shopImages.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        View v;
+        if (convertView == null) {
+            LayoutInflater inflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v=inflater.inflate(R.layout.shop_grid_item, viewGroup, false);
+        }
+        else{
+            v=convertView;
+        }
+
+        TextView textHeading = v.findViewById(R.id.shopHeading);
+        ImageView thumbnailImage = v.findViewById(R.id.shopImage);
+        thumbnailImage.setLayoutParams(new LinearLayout.LayoutParams(480, 480));
+        textHeading.setText(imageLabels[position]);
+        thumbnailImage.setImageResource(shopImages[position]);
+        return v;
+    }
+}
