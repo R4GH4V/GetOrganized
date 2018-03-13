c35b490da5dd20a121e509b3d47eb9cfbbbe40b4
r4gh4v <raghavxb@gmail.com>
2018-03-12 22:29:17 -0700

    Settings.java-> Language radio buttons are functional.

--- app/src/main/java/com/example/android/getorganized/SettingsFragment.java
+++ app/src/main/java/com/example/android/getorganized/SettingsFragment.java
@@ -1,21 +1,61 @@
 package com.example.android.getorganized;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 
+import java.util.Locale;
 
 
 // Menu - Settings
 
 public class SettingsFragment extends Fragment {
     @Nullable
-    @Override
+
+
+    private RadioButton eng,chi;
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getActivity().setTitle(R.string.settings);
         return inflater.inflate(R.layout.fragment_settings,null);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        eng=getActivity().findViewById(R.id.eng);
+        chi=getActivity().findViewById(R.id.chi);
+
+        eng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setLocale("en");
+            }
+        });
+
+        chi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setLocale("zh");
+            }
+        });
+    }
+    public void setLocale(String lang)
+    {
+        Locale locale= new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = locale;
+        res.updateConfiguration(conf, dm);
+    }
+
 }
